683fc410e8e9887d8fe891234fed9b3a37b07c12
Mati Melkamu <matimelkamu15@gmail.com>
2025-08-20 22:30:28 +0300

    fix desktop search issue and background color

--- src/pages/home/Sidebar_Section/Search/DesktopSearch/DesktopSearch.jsx
+++ src/pages/home/Sidebar_Section/Search/DesktopSearch/DesktopSearch.jsx
@@ -18,15 +18,15 @@ function DesktopSearch({ setSearchResult }) {
   };
 
   return (
-    <div className="hidden sm:flex items-center gap-2 mt-2 rounded-full border bg-slate-100 p-1  ">
+    <div className="hidden sm:flex items-center gap-2 mt-2 rounded-full border border-gray-900 bg-slate-700 p-1  ">
       <input
         type="text"
         onKeyDown={(e) => handleSearch(e)}
         onChange={(e) => setSearch(e.target.value)}
         value={search}
-        className="w-full text-black pl-2 bg-transparent border-none focus:outline-none"
+        className="w-full text-white pl-2 bg-transparent border-none focus:outline-none"
       />
-      <div className="bg-white text-black p-2 rounded-full">
+      <div className="bg-slate-500 text-black p-2 rounded-full">
         <Search className="size-4" />
       </div>
     </div>
